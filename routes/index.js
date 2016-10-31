b50f64b19ceec9fa080312552b7e02f61cbba5ca
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-31 22:57:34 +0000

    Display users and their listings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.User.findAll({
+    include: [models.Listing]
+  }).then(function(users) {
+    res.render('index', {
+      title: 'MakersBnB',
+      users: users
+    });
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* POST new user. */
+router.post('/create', function(req, res) {
+  models.User.create({
+    username: req.body.username
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+/* POST new listing for user. */
+router.post('/:user_id/listings/create', function (req, res) {
+  models.Listing.create({
+    UserId: req.params.user_id,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price
+  }).then(function() {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
