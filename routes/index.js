12a440415f8c29275e66655e6164796b69372ef2
Laurent Bouhnik <misterbl@hotmail.com>
2016-11-01 16:02:30 +0000

    new directory working

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var Listing = require('../models').Listing;
 
-/* GET home page. */
+/* GET home page/listings. */
 router.get('/', function(req, res, next) {
-  models.User.findAll({
-    include: [models.Listing]
-  }).then(function(users) {
-    res.render('index', {
-      title: 'MakersBnB',
-      users: users
-    });
+  res.render('index');
+});
+
+router.post('/user/create', function(req, res) {
+    models.User.create({
+    username: req.body.username,
+    email: req.body.email,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  });
+    res.redirect('/');
+});
+
+router.get('/new_listing', function(req, res) {
+  models.Listing.findAll({}).then(function(listings) {
+    res.json(listing);
   });
 });
+
+router.post('/new_listing', function(req, res) {
+    models.Listing.create({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    image: req.body.image,
+  });
+
+});
+
 
 module.exports = router;
